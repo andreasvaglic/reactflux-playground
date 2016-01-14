8b4f737b43445ccc11f45059b30426b53467927c
Andrea Svaglic <andrea.svaglic@hotmail.com>
2016-01-14 06:22:38 +0100

    Added Bootstrap and jQuery

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+// both "$" and "jQuery" reference to jquery
+$ = jQuery = require("jquery");
+
 // This wont be a global code, but its own module because of commonjs we use
 var App = console.log("Hello world from Browserify");
 
