f7784a91cfb1af84460a6badcf0012110c1cf723
andreasvaglic <andrea.svaglic@gmail.com>
2016-01-18 11:11:49 +0100

    added validation to the form, added proptypes, enabled editing authors

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -12,20 +12,51 @@ var ManageAuthorPage = React.createClass({
         Router.Navigation
     ],
 
+    statics: {
+        // comes with react router
+        willTransitionFrom: function (transition, component) {
+            if (component.state.dirty && !confirm("Leave without saving?")) {
+                transition.abort();
+            }
+        }
+    },
+
     getInitialState: function () {
         return {
             author: {
                 id: "",
                 firstName: "",
                 lastName: ""
-            }
+            },
+            errors : {},
+            dirty: false
         };
+    },
+
+    // React lifecycle function.
+    // Called by convention before the component has been mounted.
+    // Here it is better to use it then componentDidMount because calling setState 
+    // in this fn will not cause the re-render
+    componentWillMount: function () {
+        var authorId = this.props.params.id; // From the path '/author:id'
+
+        // Check needed because this segment wont be in url when we are adding the author
+        // remember, this component is used both for adding and for editing authors
+        if (authorId) {
+            this.setState({
+                author: AuthorApi.getAuthorById(authorId)
+            });
+        }
     },
 
     // Called for every key press
     setAuthorState: function (event) {
         var field = event.target.name,
             value = event.target.value;
+
+        this.setState({
+            dirty: true
+        });
 
         this.state.author[field] = value;
 
@@ -34,23 +65,53 @@ var ManageAuthorPage = React.createClass({
         });
     },
 
+    authorFormIsValid: function () {
+        var formIsValid = true;
+        this.state.errors = {}; // Clear any previous errors
+
+        if (this.state.author.firstName.length < 3) {
+            this.state.errors.firstName = "First name must be at least 3 characters";
+            formIsValid = false;
+        }
+
+        if (this.state.author.lastName.length < 3) {
+            this.state.errors.lastName = "Last name must be at least 3 characters";
+            formIsValid = false;
+        }
+
+        // Any time we want to change state, we need to call .setState() to do so
+        this.setState({
+            errors: this.state.errors
+        });
+
+        return formIsValid;
+    },
+
     saveAuthor: function (event) {
         event.preventDefault();
+
+        if (!this.authorFormIsValid()) {
+            return;
+        }
+
         // Save our author to our mock API
         AuthorApi.saveAuthor(this.state.author);
-
+        this.setState({
+            dirty: false
+        });
         toastr.success("Author saved.");
-
         // transitionTo is available only when using Navigaiton mixin
         this.transitionTo("authors");
     },
 
+    // If a required prop is omitted, the message is logged in browser
     render: function () {
         return (
             <AuthorForm 
                 author={this.state.author}
                 onChange={this.setAuthorState}
-                onSave={this.saveAuthor} />
+                onSave={this.saveAuthor}
+                errors={this.state.errors} />
         );
     }
 });
--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,7 @@ var routes = (
         <DefaultRoute handler={require("./components/homePage")} />
         <Route name="authors" handler={require("./components/authors/authorPage")} />
         <Route name="addAuthor" path="author" handler={require("./components/authors/manageAuthorPage")} />
+        <Route name="manageAuthor" path="author/:id" handler={require("./components/authors/manageAuthorPage")} />
         <Route name="about" handler={require("./components/about/aboutPage")} />
         <NotFoundRoute handler={require("./components/notFoundPage")} />
         <Redirect from="about-us" to="about" />
@@ -20,5 +21,7 @@ var routes = (
         <Redirect from="about/*" to="about" />
     </Route>
 );
+
+// path="author/:id" -> id is a placeholder
 
 module.exports = routes;
